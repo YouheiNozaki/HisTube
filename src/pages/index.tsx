f460b855728cb361384b4fec97e35af990bda381
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-11-03 11:17:47 +0900

    tailwindcssの設定とデフォルトのテーマを設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,12 @@ export default function Home(): React.ReactNode {
   return (
     <div className="hero">
       <h1 className="title">Next.js + Tailwind CSS</h1>
-      <h1 className="text-center text-teal-500 text-2xl py-4">
+      <h1 className="text-center text-tertiary text-2xl py-4">
         This is an Example.
       </h1>
+      <button className="btn btn-black" type="button">
+        ボタン
+      </button>
     </div>
   );
 }
