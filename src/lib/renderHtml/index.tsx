5204a16a517653230af51fc3faae6052f6fc5757
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-11-07 23:06:25 +0900

    renderHtmlの型エラーを解消

--- src/lib/renderHtml/index.tsx
+++ src/lib/renderHtml/index.tsx
@@ -1,23 +1,22 @@
-/* eslint-disable */
-import { createElement, Fragment } from 'react';
+import React from 'react';
 import unified from 'unified';
 import parse from 'rehype-parse';
 import rehypeReact from 'rehype-react';
 
 import { H1 } from './parts';
 
-// @ts-ignore
 export function renderAst(content: string): React.ReactNode {
   const htmlAst = unified()
     .use(parse, { fragment: true })
     .use(parse)
     .use(rehypeReact, {
-      createElement: createElement,
-      Fragment: Fragment,
+      createElement: React.createElement,
+      Fragment: React.Fragment,
       components: {
         h1: H1,
       },
     })
     .processSync(content).result;
+
   return htmlAst;
 }
